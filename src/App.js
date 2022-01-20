4a60a156d63063ce7f0f517d3d877d5d4ad0947d
Ryan O'Reilly <roreilly@pointb.com>
2022-01-20 11:05:07 -0800

    adding confetti

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Flex, Center, VStack, Heading, Spacer, Button } from '@chakra-ui/react'
 import { collection, doc, updateDoc, onSnapshot } from "firebase/firestore";
 import { db } from './firebase'
 import { getAuth, signInWithPopup, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import Confetti from 'react-confetti'
 
 function App() {
 
@@ -75,6 +76,7 @@ function App() {
           {dollars.map((child) => {
             let dollarColor
             let dollarSize
+            let confetti = false
             if (child.dollars === 0) {
               dollarColor = '#000'
               dollarSize = '2xl'
@@ -86,6 +88,7 @@ function App() {
             if (child.dollars >= 10) {
               dollarColor = '#1fd100'
               dollarSize = '3xl'
+              confetti = true
             }
             if (child.dollars < 0) {
               dollarColor = '#bf0000'
@@ -93,6 +96,13 @@ function App() {
             }
             return (
               <VStack w="300px" p="20px 0" key={child.id}>
+                <Confetti
+                  recycle={false}
+                  run={confetti}
+                  numberOfPieces={700}
+                  tweenDuration={10000}
+                  initialVelocityY={10, 20}
+                />
                 <Heading as="h4" size='md' fontWeight="normal" borderBottom="1px solid">{child.name}'s Target Bucks</Heading>
                 <Spacer />
                 <Heading color={dollarColor} as="h1" size={dollarSize}>${child.dollars}</Heading>
